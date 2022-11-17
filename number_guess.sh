d090d554bf6f9bffd264fa7e1a26670c1c45ba64
pastrue <patrick.ryan.sturm@gmail.com>
2022-11-17 16:05:26 +0000

    feat: progress on bash script and added database dump

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo -e "\nEnter your username:"
+read UN
+
+QUN=$($PSQL "select username from users where username='$UN'")
+
+if [[ -z $QUN ]]
+then
+  echo -e "\nWelcome, $UN! It looks like this is your first time here."
+  INUN=$($PSQL "insert into users(username) values('$UN')")
+else
+  echo 
+fi  
